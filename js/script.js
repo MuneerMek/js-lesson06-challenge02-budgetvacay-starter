a7ed69de4eff196f8295f73aa538f49274227006
M_Mek <mmekraz781@gmail.com>
2023-10-17 13:45:06 -0400

    finalized budget calc script

--- js/script.js
+++ js/script.js
@@ -9,9 +9,12 @@ var tripInfo = function () {
     prompt(`What are your accomodation costs? (i.e. 399.99)`)
   );
   var numDays = Number(prompt(`How many days will your trip last? (i.e. 12)`));
+  calculateDailyBudget(totalBudget, accomodation, numDays);
 };
 
 var calculateDailyBudget = function (totalBudget, accomodation, numDays) {
   var daily = ((totalBudget - accomodation) / numDays).toFixed(2);
   dailyBudget.innerText = `You can spent up to $${daily} each day, however you'd like! Enjoy your stay~`;
 };
+
+tripInfoButton.addEventListener("click", tripInfo);
